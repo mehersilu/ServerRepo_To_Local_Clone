f4728b8c332a1ea49f92bf538c953737d8b1fd7a
mehersilu <meher.silu@gmail.com>
2023-01-30 15:08:27 +0530

    Add project files.

---
+++ ServerRepo_To_Local_Clone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerRepo_To_Local_Clone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
